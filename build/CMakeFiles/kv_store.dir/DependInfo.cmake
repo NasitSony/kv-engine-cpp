26b9141de4bc824652dc7205aceaf66de5f5d8e5
Nasit Sarwar Sony <nasitsony96@gmail.com>
2026-03-10 12:41:36 -0700

    Flatten Repo

---
+++ build/CMakeFiles/kv_store.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nasit/dev/kv-engine-cpp/kv-cpp/src/kv_store.cpp" "CMakeFiles/kv_store.dir/src/kv_store.cpp.o" "gcc" "CMakeFiles/kv_store.dir/src/kv_store.cpp.o.d"
+  "/Users/nasit/dev/kv-engine-cpp/kv-cpp/src/raft.cpp" "CMakeFiles/kv_store.dir/src/raft.cpp.o" "gcc" "CMakeFiles/kv_store.dir/src/raft.cpp.o.d"
+  "/Users/nasit/dev/kv-engine-cpp/kv-cpp/src/raft_transport.cpp" "CMakeFiles/kv_store.dir/src/raft_transport.cpp.o" "gcc" "CMakeFiles/kv_store.dir/src/raft_transport.cpp.o.d"
+  "/Users/nasit/dev/kv-engine-cpp/kv-cpp/src/wal.cpp" "CMakeFiles/kv_store.dir/src/wal.cpp.o" "gcc" "CMakeFiles/kv_store.dir/src/wal.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
